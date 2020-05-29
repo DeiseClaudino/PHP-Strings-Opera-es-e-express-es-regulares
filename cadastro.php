94617cd98fa1c7822eb8b7d7ef7a51ece2f50b5e
DeiseClaudino <deise.claudino98@gmail.com>
2020-05-28 22:38:59 -0300

    Dividindo uma string

--- cadastro.php
+++ cadastro.php
@@ -8,18 +8,21 @@
     <title>Curso Strings</title>
 </head>
 <body>
+<?php
+    $nomeSobrenome = explode(" ", $_POST['nome'], 2);
+?>
 
 <div class="mx-5 my-5">
 <h1>Cadastro feito com sucesso.</h1>
 <p>Seguem os dados de sua conta:</p>
 <ul class="list-group">
-    <li class="list-group-item">Primeiro nome: </li>
-    <li class="list-group-item">Sobrenome: </li>
-    <li class="list-group-item">Usuário: </li>
-    <li class="list-group-item">Senha: </li>
-    <li class="list-group-item">Telefone: </li>
-    <li class="list-group-item">Email: </li>
-    <li class="list-group-item">Endereço: </li>
+    <li class="list-group-item">Primeiro nome: <?= $nomeSobrenome[0] ?></li>
+    <li class="list-group-item">Sobrenome: <?= $nomeSobrenome[1] ?> </li>
+    <li class="list-group-item">Usuário: <?= $_POST['nome'] ?></li>
+    <li class="list-group-item">Senha: <?= $_POST['senha'] ?></li>
+    <li class="list-group-item">Telefone: <?= $_POST['telefone'] ?></li>
+    <li class="list-group-item">Email: <?= $_POST['email'] ?></li>
+    <li class="list-group-item">Endereço: <?= $_POST['nome'] ?></li>
 </ul>
 </div>
 </body>
